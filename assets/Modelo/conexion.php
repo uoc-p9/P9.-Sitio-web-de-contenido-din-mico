326bcf7c69eb8b15128459eeaa28825133f6af12
LAPTOP-NPPFVRM4\javic <jcamarenam@fp.uoc.edu>
2019-04-20 17:37:33 +0200

    prueba

--- assets/Modelo/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Conexion{
-    public static function Conectar(){
-        $pdo = new PDO('mysql:host=localhost;dbname=valoraciones_cerveza;charset=utf8', 'root', '');
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
-        return $pdo;
-    }
-}
-?>
